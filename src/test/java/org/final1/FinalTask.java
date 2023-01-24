95d8afd7cbb7853d9b1cea86d29a5cd6a37a1556
Chitradevig <chituvicha@gmail.com>
2023-01-24 20:58:12 +0530

    first codes

---
+++ src/test/java/org/final1/FinalTask.java
@@ -0,0 +1,16 @@
+package org.final1;
+
+public class FinalTask {
+private void method1() {
+	System.out.println("started");
+
+}
+private void method2() {
+System.out.println("started");	
+
+}
+private void method3() {
+	System.out.println("started");
+
+}
+}
